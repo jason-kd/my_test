ffbbdcdc0cd27cc20b1c088128e347db9554c6d6
jason-kd <58615476+jason-kd@users.noreply.github.com>
2023-12-17 23:22:56 +0800

    Dev/chenguangjian/test tmp (#14)
    test1

--- Uart/uart_mgr.c
+++ Uart/uart_mgr.c
@@ -38,9 +38,9 @@ static bool uart1_start = false;
 
 uint8_t get_uart_last_cmd_ack(uint32_t block_time_ms)
 {
-	
-	int s32Test[10] = {0}
-	
+	int as32Test[10] = {0};
+
+	as32Test[10] = 12;
 	
 	uint8_t ack = 0;
 
