4e5f714e3cc6f58a7bb602dd4571068127f60816
jason-wyze <jason.chen@wyze.com>
2024-01-16 01:10:45 +0800

    test-0116v2

--- .github/workflows/cross_commit_check.py
+++ .github/workflows/cross_commit_check.py
@@ -1,6 +1,12 @@
 import subprocess
 import sys
 
+if len(sys.argv) < 4:
+    args_count = len(sys.argv) - 1  # 减去脚本名称本身
+    print("命令行参数个数:", args_count)
+    print("Error: Insufficient command line arguments")
+    print("Usage: python cross_commit_check.py REPO_OWNER REPO_NAME PR_NUMBER")
+    sys.exit(1)
 
 REPO_OWNER = sys.argv[1]
 REPO_NAME = sys.argv[2]
